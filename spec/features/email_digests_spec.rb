396f4ad15068046efc9a41dd8fef12af6b6848c1
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-22 12:22:16 -0700

    update spec information

--- spec/features/email_digests_spec.rb
+++ spec/features/email_digests_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-feature 'Email Digests' do
+feature 'Events Trigger Responses' do
 
-  scenario "sending an email digest for a single daily event" do
+  scenario "daily digest scenario" do
     user = User.create(username: 'test_user', email: 'test@test.com')
     scenario = Scenario.create(frequency: "daily", event_name: "Tomorrow's Weather", user: user, threshold: 1, action: 'digest')
     payload = { name: "Tomorrow's Weather", count: 1, next_call: 1.day.from_now, data: { text: "It's going to be hot" } }.to_json
     event = ProcessPayload.new(payload).create_event
-    expect { EventTrigger.new(event).trigger_responses }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    expect { EventTrigger.new(event).trigger_responses }.to change(Response, :count).by(1)
   end
 end
