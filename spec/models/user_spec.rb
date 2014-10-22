bcecf0f989f1a436264168697568912e4a688e6b
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-22 16:37:45 -0700

    use devise back user record

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-
-  before(:each) do
-    @attr = { username: 'Test Name', email: 'test@gmail.com'}
-  end
-
-  it "is valid with all attributes" do
-    u = User.new(@attr)
-    expect(u).to be_valid
-  end
-
-  it "requires a name" do
-    @attr.delete(:username)
-    u = User.new(@attr)
-    expect(u).to_not be_valid
-  end
-
-  it "requires an email" do
-    @attr.delete(:email)
-    u = User.new(@attr)
-    expect(u).to_not be_valid
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
