0787aa7e280826a770eff839ccb671ace64b20ed
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-22 16:59:45 -0700

    update spec to comply with devise

--- spec/features/email_digests_spec.rb
+++ spec/features/email_digests_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Events Trigger Responses' do
 
   scenario "daily digest scenario" do
-    user = User.create(username: 'test_user', email: 'test@test.com')
+    user = User.create!(email: 'test@test.com', password: 'mypassword', password_confirmation: 'mypassword')
     scenario = Scenario.create(frequency: "daily", event_name: "Tomorrow's Weather", user: user, threshold: 1, action: 'digest')
     payload = { name: "Tomorrow's Weather", count: 1, next_call: 1.day.from_now, data: { text: "It's going to be hot" } }.to_json
     event = ProcessPayload.new(payload).create_event
