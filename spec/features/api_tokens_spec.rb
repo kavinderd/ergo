b3702437316e06dc15b9fce46dcdaaff65c410e9
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-10-30 13:59:01 -0700

    token feature spec ought to actually test that the token is there

--- spec/features/api_tokens_spec.rb
+++ spec/features/api_tokens_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 feature "Api Tokens" do
 
   background do
-    u = User.create(email: 'test@test.com', password: 'mypassword', password_confirmation: 'mypassword')
+    @u = User.create(email: 'test@test.com', password: 'mypassword', password_confirmation: 'mypassword')
     visit '/users/sign_in'
-    fill_in 'user_email', with: u.email
+    fill_in 'user_email', with: @u.email
     fill_in 'user_password', with: 'mypassword'
     click_button 'Sign in'
   end
@@ -13,7 +13,7 @@ feature "Api Tokens" do
   scenario "Creating a new token" do
     expect(page).not_to have_content("Your Api Token")
     click_button "Get Api Token"
-    expect(page).to have_content "Api Token: "
+    expect(page).to have_content("#{@u.reload.token}")
     expect(page).not_to have_content "Get Api Token"
   end
 
